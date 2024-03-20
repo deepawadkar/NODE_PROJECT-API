b3cd4565942f4e908e0d295569dc520fcbcb35c3
deepa <deepawadkar2002@gmail.com>
2024-03-20 08:12:57 +0530

    first commit

---
+++ controller/userCtrl.js
@@ -0,0 +1,14 @@
+const { StatusCode, StatusCodes } = require('http-status-codes')
+const UserModel = require('../model/user')
+
+// read alluser info except admin
+const readAllUsers = async (req,res) => {
+    try {
+        res.json({ msg: "read all users"})
+    } catch(err) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ status :false, msg: err.message})
+    }
+}
+
+module.exports = { readAllUsers }
+
---
+++ middleware/auth.js
@@ -0,0 +1,32 @@
+ //auth middleware
+ const { StatusCodes } = require("http-status-codes")
+ const jwt = require('jsonwebtoken')
+
+ const auth = async (req,res,next) => {
+    try {
+        // read the token from header
+        const token = req.header('Authorization')
+
+        // verify the token
+        await jwt.verify(token,process.env.ACCESS_SECRET, (err,user) => {
+            if(err)
+            return res.status(StatusCodes.UNAUTHORIZED).json({ status: false, msg: `unauthorized token or token expires`})
+
+            //if valid token it will return user id
+           //res.json({ user })
+
+           // create req.const
+           req.userId = user.id
+
+           // continue connection to main controller
+           next()
+        })
+
+        // verify the token
+        //res.json({ middle : token })
+    }catch(err) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ status: false, msg: err.message})
+    }
+ }
+
+ module.exports = auth
